fb520efd81fd23287b08fe22d08a7335b9c23ef2
Zied.mathlouthi@esprit.tn <zied.mathlouthi@esprit.tn>
2023-10-02 09:17:53 +0100

    project Validé

---
+++ src/main/java/com/example/candidate/CandidateApplication.java
@@ -0,0 +1,28 @@
+package com.example.candidate;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class CandidateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandidateApplication.class, args);
+    }
+    @Bean
+    ApplicationRunner init(CandidatRepository repository) {
+        return args -> {
+            Stream.of("Mariem", "Sarra", "Mohamed").forEach(nom -> {
+                repository.save(new Candidat(nom));
+            });
+            repository.findAll().forEach(System.out::println);
+        };
+    }
+}
